30b06e0732c2827e4e883dffca33b62872fc9f5d
Кира Монёв <kamonev@edu.hse.ru>
2026-05-02 22:46:01 +0300

    fix: add backend module to python path in alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,5 +1,10 @@
 import asyncio
+import os
+import sys
 from logging.config import fileConfig
+
+# Add the backend directory to sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from alembic import context
 from sqlalchemy import pool
